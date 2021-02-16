06ed513814f4c356d0b7e1c66e7f85042ed14e63
tikhomirova-a <tikhomirova.aa95@gmail.com>
2021-02-16 18:11:26 +0300

    add js to the modal

--- source/js/main.js
+++ source/js/main.js
@@ -42,8 +42,86 @@
     }
   };
 
-  for (let header of headers) {
-    header.addEventListener(`click`, onHeaderClick);
-    header.addEventListener(`keydown`, onHeaderEnter);
+  if (headers) {
+    for (let header of headers) {
+      header.addEventListener(`click`, onHeaderClick);
+      header.addEventListener(`keydown`, onHeaderEnter);
+    }
+  }
+})();
+
+(function () {
+  let body;
+  let feedbackLink;
+  let modal;
+  let modalContent;
+  let modalClose;
+
+  if (document.querySelector(`.body`)) {
+    body = document.querySelector(`.body`);
+
+    if (body.querySelector(`.page-header__feedback a`)) {
+      feedbackLink = body.querySelector(`.page-header__feedback a`);
+    }
+
+    if (body.querySelector(`.modal`)) {
+      modal = body.querySelector(`.modal`);
+
+      if (modal.querySelector(`.modal__content`)) {
+        modalContent = modal.querySelector(`.modal__content`);
+
+        if (modalContent.querySelector(`#close-modal`)) {
+          modalClose = modalContent.querySelector(`#close-modal`);
+        }
+      }
+    }
+  }
+
+  const showModal = () => {
+    modal.classList.add(`modal--open`);
+    modalClose.addEventListener(`click`, onModalCloseClick);
+    modalContent.addEventListener(`click`, onModalContentClick);
+    body.addEventListener(`keydown`, onModalEsc);
+    modal.addEventListener(`click`, onModalOverlayClick);
+    body.classList.add(`body--modal`);
+  };
+
+  const hideModal = () => {
+    modal.classList.remove(`modal--open`);
+    modalClose.removeEventListener(`click`, onModalCloseClick);
+    modalContent.removeEventListener(`click`, onModalContentClick);
+    body.removeEventListener(`keydown`, onModalEsc);
+    modal.removeEventListener(`click`, onModalOverlayClick);
+    body.classList.remove(`body--modal`);
+  };
+
+  const onFeedbackLinkClick = (evt) => {
+    evt.preventDefault();
+    showModal();
+  };
+
+  const onModalCloseClick = () => {
+    hideModal();
+  };
+
+  const onModalEsc = (evt) => {
+    if (evt.key === `Escape`) {
+      evt.preventDefault();
+      hideModal();
+    }
+  };
+
+  const onModalOverlayClick = (evt) => {
+    if (evt.currentTarget.className === `modal modal--open`) {
+      hideModal();
+    }
+  };
+
+  const onModalContentClick = (evt) => {
+    evt.stopPropagation();
+  };
+
+  if (feedbackLink) {
+    feedbackLink.addEventListener(`click`, onFeedbackLinkClick);
   }
 })();
